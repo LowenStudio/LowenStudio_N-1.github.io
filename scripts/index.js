7122a58f5893bb2e4003817f2736afe62b5b364d
Yunwon.Lee <60532610+yunwonlee@users.noreply.github.com>
2021-07-23 15:35:41 +0900

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 let userAgent = navigator.userAgent;
 if((userAgent .match(/safari/i))&&!(userAgent .match(/chrome|chromium|crios/i))){
-    alert("safari");
+    console.log(userAgent);
 } else {
     location.replace("/404/404.html");
 }
